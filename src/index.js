bec75d3e580f28bc22c6748ccf1c83b34a7a54d1
ViktoriiaYermak <yermakvictoriia@gmail.com>
2023-09-18 19:03:44 +0300

    feat: update file JS in folder src

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  const nStr = String(n);
+  const reversStr = nStr.split('').reverse().join('');
+  const reversNum = parseInt(reversStr);
+
+  return reversNum;
 }
